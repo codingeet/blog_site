a6865fea3f88e6edefd775204885b44bf8650b0d
codingeet <geet2136@gmail.com>
2025-07-11 19:09:48 +0530

    header component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+  <BrowserRouter>
+      <Header/>
       <Home />
-    </div>
+   </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+ import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+// import "../styles/theme.css";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  return (
+    <header className="site-header">
+      <div className="logo">
+        <Link to="/">SmartBlog</Link>
+      </div>
+
+      <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <Link to="/">Home</Link>
+        <Link to="/blogs">Blogs</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+    </header>
+  );
+};
+
+export default Header;

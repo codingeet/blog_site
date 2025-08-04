eeb37d8242968cd4301007c44ab20d118610a4c7
Geetanjali <geet2136@gmail.com>
2025-08-04 12:00:24 +0530

    dangerouslySetInnerHtml implimented

--- frontend/src/pages/DetailBlog.jsx
+++ frontend/src/pages/DetailBlog.jsx
@@ -3,21 +3,29 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../styles/DetailBlog.css";
 
-const DetailBlog = (props)=> {
-  const { id } = useParams(); // 👈 get blog id from URL
-  const [blog, setBlog] = useState(null);
+const DetailBlog = (props) => {
+    const { id } = useParams(); // 👈 get blog id from URL
+    const [blog, setBlog] = useState(null);
 
-  useEffect(() => {
-    // Fetch blog using the id
-    fetch(`http://localhost:5000/api/blogs/${id}`)
-      .then((res) => res.json())
-      .then((data) => setBlog(data))
-      .catch((err) => console.error("Error fetching blog:", err));
-  }, [id]);
-console.log(blog,'>>>>>>>>>>>>>>>');
+    useEffect(() => {
+        // Fetch blog using the id
+        fetch(`http://localhost:5000/api/blogs/${id}`)
+            .then((res) => res.json())
+            .then((data) => setBlog(data))
+            .catch((err) => console.error("Error fetching blog:", err));
+    }, [id]);
+    console.log(blog, '>>>>>>>>>>>>>>>');
     return (
         <div className="detail-wrapper">
             <h3 className="blog-title">{blog?.title}</h3>
+            <div className="img-wrap">
+                <img src={blog?.thumbnail} />
+            </div>
+            <div className="content">
+                <div dangerouslySetInnerHTML={{ __html: blog?.content }}>
+
+                </div>
+            </div>
         </div>
     )
 };

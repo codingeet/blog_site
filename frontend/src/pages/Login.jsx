638446fbac9059b46dac8e6c29961fd1caaa18c5
Geetanjali <geet2136@gmail.com>
2025-08-06 18:08:17 +0530

    signup styling updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
                             <input type="text" id="email" name="email" placeholder="Enter your Email" />
                         </div>
                         <div className='input-wrap'>
-                            <input type="text" id="password" name="password" placeholder="Enter your Password" />
+                            <input type="password" id="password" name="password" placeholder="Enter your Password" />
                         </div>
                         <div className="button-wrapper">
                             <button className="btn btn-primary  submit" type='button'>Login</button>
@@ -24,10 +24,19 @@ export default function Login() {
                     :
                     <div className="form">
                         <h3>SignUp Form</h3>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <input type="password" placeholder=" Confirm Password" />
-                        <button className="btn btn-primary  submit" type='button'>SignUp</button>
+                       <div className='input-wrap'>
+                            <input type="text" id="email" name="email" placeholder="Enter your Email" />
+                        </div>
+                        <div className='input-wrap'>
+                            <input type="password" id="password" name="password" placeholder="Enter your Password" />
+                        </div>
+                         <div className='input-wrap'>
+                            <input type="password" id="confirmpassword" name="Confirmpassword" placeholder="Confirm Password" />
+                        </div>
+                        <div className="button-wrapper">
+                            <button className="btn btn-primary  submit" type='button'>SignUp</button>
+                        </div>
+                        
                         <p>Already have an account ? <span className="link-button" onClick={() => toggleLoginForm(true)}> SignIn</span></p>
                     </div>
                 }

6fbe0523dfb09ce9a2d4c13dbf0e9f62deba7b42
Geetanjali <geet2136@gmail.com>
2025-08-07 21:06:32 +0530

    eye icon  added on login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import "../styles/Login.css"
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+
+
 
 export default function Login() {
     const [isLoginForm, toggleLoginForm] = useState(true);
+    const [openEye, toggleOpenEye] = useState(false);
+    const handleClick = () => {
+        toggleLoginForm(!isLoginForm)
+    }
     return (
         <div className="container">
             <div className="form-container">
@@ -13,7 +20,8 @@ export default function Login() {
                             <input type="text" id="email" name="email" placeholder="Enter your Email" />
                         </div>
                         <div className='input-wrap'>
-                            <input type="password" id="password" name="password" placeholder="Enter your Password" />
+                            <input id="password" name="password" type={openEye ? "text" : "password"} placeholder="Enter your Password" className="ml-[-5vh] text-xl cursor-pointer hover:text-[red] duration-300" />
+                            <span onClick={() => toggleOpenEye(!openEye)} className="eye-icon"> {openEye ? <IoEyeOutline /> : <IoEyeOffOutline />}</span>
                         </div>
                         <div className="button-wrapper">
                             <button className="btn btn-primary  submit" type='button'>Login</button>
@@ -24,19 +32,19 @@ export default function Login() {
                     :
                     <div className="form">
                         <h3>SignUp Form</h3>
-                       <div className='input-wrap'>
+                        <div className='input-wrap'>
                             <input type="text" id="email" name="email" placeholder="Enter your Email" />
                         </div>
                         <div className='input-wrap'>
                             <input type="password" id="password" name="password" placeholder="Enter your Password" />
                         </div>
-                         <div className='input-wrap'>
+                        <div className='input-wrap'>
                             <input type="password" id="confirmpassword" name="Confirmpassword" placeholder="Confirm Password" />
                         </div>
                         <div className="button-wrapper">
                             <button className="btn btn-primary  submit" type='button'>SignUp</button>
                         </div>
-                        
+
                         <p>Already have an account ? <span className="link-button" onClick={() => toggleLoginForm(true)}> SignIn</span></p>
                     </div>
                 }
